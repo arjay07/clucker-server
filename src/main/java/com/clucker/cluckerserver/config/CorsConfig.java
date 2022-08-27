05100b4af9761a287663942c44a64ccd34260aaf
arjay07 <arjay.07.ly@gmail.com>
2022-08-27 10:01:32 -0600

    Change cors config to use CorsConfigurationSource bean

--- src/main/java/com/clucker/cluckerserver/config/CorsConfig.java
+++ src/main/java/com/clucker/cluckerserver/config/CorsConfig.java
@@ -1,9 +1,14 @@
 package com.clucker.cluckerserver.config;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
 
 @Configuration
 @RequiredArgsConstructor
@@ -11,8 +16,17 @@ public class CorsConfig implements WebMvcConfigurer {
 
     private final AppProperties appProperties;
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedOrigins(appProperties.getAllowedOrigins());
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+
+        final CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(List.of(appProperties.getAllowedOrigins()));
+        corsConfiguration.setAllowedMethods(List.of("GET", "POST", "UPDATE", "DELETE", "PUT"));
+        corsConfiguration.setAllowedHeaders(List.of("Authorization", "Content-Type", "Cache-Control"));
+        corsConfiguration.setExposedHeaders(List.of("Authorization", "Location"));
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", corsConfiguration);
+        return source;
+
     }
 }
